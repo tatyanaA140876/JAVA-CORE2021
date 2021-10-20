4a306737b3e4e29f81a145c3e55279fb10c82d00
tatyana <androsova14081976@gmail.com>
2021-10-20 13:12:24 +0300

    HomeWork1

---
+++ src/lesson1/Team.java
@@ -0,0 +1,22 @@
+package lesson1;
+
+
+
+public class Team {
+    String teamName;
+    Athlete[] team;
+
+
+    public Team(Athlete[] team, String teamName) {
+        this.teamName = teamName;
+        this.team = team;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public Athlete[] getTeam() {
+        return team;
+    }
+}
